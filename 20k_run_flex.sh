219412e43a68ac1c144fe53cebb9961bcf8929e8
HaydonL <phl19@ic.ac.uk>
2023-07-12 16:55:04 +0100

    Add R & shell code to run flexible model

---
+++ 20k_run_flex.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+INDIR="/rds/general/user/phl19/home/git/phylo_households_dissertation"
+OUTDIR="/rds/general/user/phl19/home/results/phylo_households_dissertation"
+
+mkdir $OUTDIR
+
+cat > $OUTDIR/bash_20k_run_flex.pbs <<EOF
+  
+#!/bin/sh
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ncpus=10:ompthreads=1:mem=240gb
+#PBS -j oe
+module load anaconda3/personal
+source activate Renv # new line added
+
+INDIR=$INDIR
+Rscript \$INDIR/src/run_20k_flex.R
+
+EOF
+
+cd $OUTDIR
+qsub bash_20k_run.pbs
---
+++ src/run_20k_flex.R
@@ -0,0 +1,20 @@
+library(rstan)
+options(mc.cores = parallel::detectCores())
+
+filename <- here::here("data", "simulated", "rline_1.csv")
+modelpath <- here::here("stan-models", "beta_mixture_DP_one_group_flex_alpha.stan")
+data <- read.csv(filename)
+
+K <- 5 # number of clusters
+
+stan_data <- list(
+  N = dim(data)[1],
+  K = K,
+  ages = data,
+  min_age = 0,
+  max_age = 1
+)
+
+fit <- stan(file = modelpath, data = stan_data, iter = 23000,
+            warmup = 3000, seed = 538164, chains = 4)
+saveRDS(fit, here::here("data", "rline_1_draws_20k_flex.rds"))
