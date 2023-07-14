0bfac182368a8e5ff4fe620247f50ffdb40248e9
HaydonL <phl19@ic.ac.uk>
2023-07-14 12:29:06 +0100

    Add sh and R code for more clusters

---
+++ 20k_run_more_cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+INDIR="/rds/general/user/phl19/home/git/phylo_households_dissertation"
+OUTDIR="/rds/general/user/phl19/home/results/phylo_households_dissertation"
+
+mkdir $OUTDIR
+
+cat > $OUTDIR/bash_20k_more_cluster.pbs <<EOF
+  
+#!/bin/sh
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ncpus=10:ompthreads=1:mem=240gb
+#PBS -j oe
+module load anaconda3/personal
+source activate Renv 
+
+INDIR=$INDIR
+Rscript \$INDIR/src/bash_20k_more_cluster.R -indir \$INDIR
+
+EOF
+
+cd $OUTDIR
+qsub bash_20k_more_cluster.pbs
